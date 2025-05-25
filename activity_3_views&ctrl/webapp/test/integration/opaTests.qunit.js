aee76dc4c6b89efe87e1e68da1c1f13bf34b3d3a
jeffreysumalde <jeffrey.sumalde2021@gmail.com>
2025-05-25 22:17:01 +0800

    Delete activity_3_views&ctrl directory

--- activity_3_views&ctrl/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["exer1sumalde/project1/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- activity_3_views&ctrl/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"exer1sumalde/project1/test/unit/controller/MainView.controller"
-], function () {
-	"use strict";
-});
